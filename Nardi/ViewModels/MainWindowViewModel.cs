5cf2d53911f23928c1ec2346963b4d3079a3d799
Aleksey <alendk.real@gmail.com>
2023-05-15 12:24:55 +0300

    Добавьте файлы проекта.

---
+++ Nardi/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Nardi.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nardi.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+
+    {
+        #region WindowTitle
+
+        private string _Title = "Нарды";
+        /// <summary> WindowTitle </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+
+}
